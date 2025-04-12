0eb074c5d5e92c288b35ee7426cead4166eb9ec0
duvallo <duvallo@oregonstate.edu>
2025-04-12 12:18:03 -0700

    Setting up mason-nvim-dap.nvim

--- init.lua
+++ init.lua
@@ -278,36 +278,73 @@ require('lazy').setup({
 
   -- Copied from https://www.johntobin.ie/blog/debugging_in_neovim_with_nvim-dap/
   {
-    "mfussenegger/nvim-dap",
+    'mfussenegger/nvim-dap',
     lazy = true,
     -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and
     -- modified.
     keys = {
       {
-        "<leader>db",
-        function() require("dap").toggle_breakpoint() end,
-        desc = "Toggle Breakpoint"
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Toggle Breakpoint',
       },
 
       {
-        "<leader>dc",
-        function() require("dap").continue() end,
-        desc = "Continue"
+        '<leader>dc',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Continue',
       },
 
       {
-        "<leader>dC",
-        function() require("dap").run_to_cursor() end,
-        desc = "Run to Cursor"
+        '<leader>dC',
+        function()
+          require('dap').run_to_cursor()
+        end,
+        desc = 'Run to Cursor',
       },
 
       {
-        "<leader>dT",
-        function() require("dap").terminate() end,
-        desc = "Terminate"
+        '<leader>dT',
+        function()
+          require('dap').terminate()
+        end,
+        desc = 'Terminate',
       },
     },
-  }
+  },
+
+  -- Copied from https://www.johntobin.ie/blog/debugging_in_neovim_with_nvim-dap/
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    ---@type MasonNvimDapSettings
+    opts = {
+      -- This line is essential to making automatic installation work
+      -- :exploding-brain
+      handlers = {},
+      automatic_installation = {
+        -- These will be configured by separate plugins.
+        exclude = {
+          'delve',
+          'python',
+        },
+      },
+      -- DAP servers: Mason will be invoked to install these if necessary.
+      ensure_installed = {
+        'bash',
+        'codelldb',
+        'php',
+        'python',
+      },
+    },
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'williamboman/mason.nvim',
+    },
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
